4229c1d400cdbd40447578d878976f0ad467f667
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-25 16:01:03 +0530

    completable future

---
+++ CompletableFuture/src/com/thread/completablefuture/runasync/RunAsync.java
@@ -0,0 +1,19 @@
+package com.thread.completablefuture.runasync;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+class RunAsync {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+	
+		System.out.println("RunAsync.main() START");
+		CompletableFuture completableFuture 	 = CompletableFuture.runAsync(() -> {
+			System.out.println("I am run async");
+		});
+		//when you dont want anything in return then use runAsync
+		System.out.println(completableFuture.join());
+		System.out.println("RunAsync.main() END");    
+	}
+
+}
